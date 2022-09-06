92bf1a4dd4709871106f9ae3fdf7228783d0796e
vanphuc <nguyen.van.phuc-b@sun-asterisk.com>
2022-09-06 07:34:44 +0700

    all app

---
+++ app/src/main/java/com/example/borutoapp/presentation/screens/search/SearchScreen.kt
@@ -0,0 +1,54 @@
+package com.example.borutoapp.presentation.screens.search
+
+import android.annotation.SuppressLint
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.paging.compose.collectAsLazyPagingItems
+import coil.annotation.ExperimentalCoilApi
+import com.example.borutoapp.presentation.common.ListContent
+import com.example.borutoapp.ui.theme.statusBarColor
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@ExperimentalCoilApi
+@Composable
+fun SearchScreen(
+    navController: NavHostController,
+    searchViewModel: SearchViewModel = hiltViewModel()
+) {
+    val searchQuery by searchViewModel.searchQuery
+    val heroes = searchViewModel.searchedHeroes.collectAsLazyPagingItems()
+    val systemUiController = rememberSystemUiController()
+    val systemBarColor = MaterialTheme.colors.statusBarColor
+
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = systemBarColor
+        )
+    }
+
+    Scaffold(
+        topBar = {
+            SearchTopBar(
+                text = searchQuery,
+                onTextChange = {
+                    searchViewModel.updateSearchQuery(query = it)
+                },
+                onSearchClicked = {
+                    searchViewModel.searchHeroes(query = it)
+                },
+                onCloseClicked = {
+                    navController.popBackStack()
+                }
+            )
+        },
+        content = {
+            ListContent(heroes = heroes, navController = navController)
+        }
+    )
+}
